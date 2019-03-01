b5f7f2a295d315c50a265c67cf740196099e91a1
Matthew Dolan <info@mattdolan.com>
2019-03-01 23:09:00 +0000

    Rename rule 21

--- lint/src/test/kotlin/com/appmattus/markdown/rules/NoMultipleSpaceClosedAtxRuleTest.kt
+++ lint/src/test/kotlin/com/appmattus/markdown/rules/NoMultipleSpaceClosedAtxRuleTest.kt
@@ -3,7 +3,7 @@ package com.appmattus.markdown.rules
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
-object MD021Test : Spek({
+object NoMultipleSpaceClosedAtxRuleTest : Spek({
     Feature("NoMultipleSpaceClosedAtxRule") {
         FileRuleScenario { NoMultipleSpaceClosedAtxRule() }
     }
