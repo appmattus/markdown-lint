3e73594547b768f71e22b4f097aa7c2dcb9ab7d1
Matthew Dolan <matt.dolan@babylonhealth.com>
2019-08-29 13:04:12 +0100

    Increase test timeout

--- markdown/src/test/kotlin/com/appmattus/markdown/plugin/MarkdownLintPluginTest.kt
+++ markdown/src/test/kotlin/com/appmattus/markdown/plugin/MarkdownLintPluginTest.kt
@@ -11,7 +11,7 @@ import java.io.InputStream
 import java.util.concurrent.TimeUnit
 
 object MarkdownLintPluginTest : Spek({
-    val timeout: Long = TimeUnit.SECONDS.toMillis(30)
+    val timeout: Long = TimeUnit.MINUTES.toMillis(1)
 
     Feature("MarkdownLintPlugin") {
         val temporaryFolder by memoized {
